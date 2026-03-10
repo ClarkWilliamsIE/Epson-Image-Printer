7861119cacafe1302dac931910ade1d4c3db0de4
Kapiti-Coast-District-Libraries <paraparaumumake@gmail.com>
2026-03-11 11:22:07 +1300

    deleted:    dist/assets/index-BSmKtavi.css 	new file:   dist/assets/index-DKxOgaRr.css 	renamed:    dist/assets/index-D5Yiy5Rd.js -> dist/assets/index-DZkCKnUe.js 	modified:   dist/index.html 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/Espon-Image-Printer/',
+    base: '/LibSysAI/',
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
