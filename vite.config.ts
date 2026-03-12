441ab27813433ff012f065c78e4dc0181018d198
ClarkWilliamsIE <clark@inertiaed.org>
2026-03-12 13:57:04 +1300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+
   return {
-    // This MUST match your GitHub repository name exactly (it is case-sensitive)
-    base: '/Epson-Image-Printer/', 
+    base: '/Epson-Image-Printer/',
     server: {
       port: 3000,
       host: '0.0.0.0',
